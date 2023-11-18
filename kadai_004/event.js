b6ca1fde80a30cecf4189ea3d9689a91c60e3ca1
ai-max16 <148667566+ai-max16@users.noreply.github.com>
2023-11-18 10:41:01 +0900

    課題004を提出

---
+++ kadai_004/event.js
@@ -0,0 +1,10 @@
+
+  // 読み込み完了した場合
+  $(window).on('load', () => {
+    console.log('loadイベントが発生しました');
+  });
+
+  // スクロールした場合
+  $(window).on('scroll', () => {
+    console.log('scrollイベントが発生しました');
+  });
